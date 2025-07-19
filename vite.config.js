85068296a8d65dd3e8be68ce81a125a27b6e5361
v-pawan <v-pawan@damacgroup.com>
2025-07-19 12:33:29 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/whoiam/", // Set the base path for GitHub Pages
 });
